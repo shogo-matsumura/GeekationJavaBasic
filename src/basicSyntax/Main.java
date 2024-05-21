dcb269587dc0f2295859dbec5b152af231d7e9cb
shogo-matsumura <s.matsumura.cingroup2012@gmail.com>
2024-05-21 14:44:09 +0900

    他クラスの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,12 @@
+package basicSyntax;
+
+public class Main {
+  public static void main(String[] args) {
+//実装内容1の呼び出し
+      GeekIntroduction.greeting("one");
+      GeekIntroduction.greeting("two");
+      GeekIntroduction.greeting("three");
+//実装内容2の呼び出し
+      GeekIntroduction.specialSkill(3,5);
+  }
+}

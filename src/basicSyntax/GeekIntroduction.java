985bd35c886891443d19eaf7093d290479c9b211
shogo-matsumura <s.matsumura.cingroup2012@gmail.com>
2024-05-23 10:42:35 +0900

    コードの修正

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -1,28 +1,16 @@
 package basicSyntax;
 
 public class GeekIntroduction {
-//実装内容1
-	public static void greeting(String type) {
-		String message = "";
-		int age = 33;
-		double height = 180;
-
-		switch (type) {
-		case "one":
-			message = "こんにちは! 私はGeek君でごわす。";
-			break;
-		case "two":
-			message = "年は" + age + "でごわす。";
-			break;
-		case "three":
-			message = "年は" + height + "でごわす。";
-			break;
-		}
-		System.out.println(message);
+	//実装内容1
+	public static void greeting(int age, double height) {
+		System.out.println("こんにちは! 私はGeek君でごわす。");
+		System.out.println("年は" + age + "でごわす。");
+		System.out.println("身長は" + height + "でごわす。");
 	}
-//実装内容2
+
+	//実装内容2
 	public static void specialSkill(int num1, int num2) {
-//num1からnumw2の範囲で繰り返される処理
+	//num1からnumw2の範囲で繰り返される処理
 		if (num1 <= 0 || num2 <= 0) {
 			System.out.println("num1とnum2は0より大きい値にしてください。");
 			return;
@@ -35,7 +23,7 @@ public class GeekIntroduction {
 			System.out.println("num1とnum2は300以下にしてください。");
 			return;
 		}
-// num1からnum2の範囲内で繰り返し処理
+	// num1からnum2の範囲内で繰り返し処理
 		for (int i = num1; i <= num2; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println(i + "は3の倍数かつ5の倍数です。");
--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -3,9 +3,7 @@ package basicSyntax;
 public class Main {
   public static void main(String[] args) {
 //実装内容1の呼び出し
-      GeekIntroduction.greeting("one");
-      GeekIntroduction.greeting("two");
-      GeekIntroduction.greeting("three");
+	  GeekIntroduction.greeting(33,180.5);
 //実装内容2の呼び出し
       GeekIntroduction.specialSkill(3,5);
   }
